850893910fb57c79f24a1dc75cf51125462a579d
Aditya Narayanan <aditya.narayanan03@gmail.com>
2024-04-25 11:52:38 -0500

    Adds test for random tiebreak

--- tests/envs/test_tictactoe.py
+++ tests/envs/test_tictactoe.py
@@ -41,19 +41,20 @@ def test_action():
     r.step({"player_1": (15, 0), "player_2": (10, 0)})
     print(r.observe("player_1"))
     print(r.env.terminations)
-    #r.render()
+    # r.render()
 
     print("Taking second round of actions")
     r.step({"player_1": (15, 1), "player_2": (10, 1)})
     print(r.observe("player_1"))
     print(r.env.terminations)
-    #r.render()
+    # r.render()
 
     print("Taking third round of actions")
     r.step({"player_1": (15, 2), "player_2": (10, 2)})
     print(r.observe("player_1"))
     print(r.env.terminations)
-    #r.render()
+    # r.render()
+
 
 def test_speed():
     """Tests speed of making plays in the game."""
@@ -74,17 +75,18 @@ def test_speed():
         r.reset()
 
         r.step({"player_1": (1, 0), "player_2": (0, 0)})
-        #print(r.env.terminations)
+        # print(r.env.terminations)
         r.step({"player_1": (1, 4), "player_2": (0, 0)})
-        #print(r.env.terminations)
+        # print(r.env.terminations)
         r.step({"player_1": (1, 8), "player_2": (0, 0)})
-        #print(r.env.terminations)
-    
+        # print(r.env.terminations)
+
     print(f"Finished {10_000} iterations in {time.time() - start_time} seconds.")
+
 
 def test_action_space():
     """Tests the action space function for an agent.
-    
+
     The action space includes the legal mask, so we test to see and make sure
     that only legal actions are returned by the action space function.
     """
@@ -97,3 +99,31 @@ def test_action_space():
     print(r.action_space("player_1"))
     print(r.action_space("player_1").sample())
     print(r.observe("player_1"))
+
+
+def test_tiebreak():
+    """Ensures that ties on bidding assign moves randomly."""
+    r = RichmanEnv(
+        env=tictactoe_v3.raw_env(render_mode="human"), capital=100, verbose=True
+    )
+
+    r.reset()
+
+    print("\n")
+    print("Taking first round of actions")
+    r.step({"player_1": (10, 0), "player_2": (10, 0)})
+    print(r.observe("player_1"))
+    print(r.env.terminations)
+    r.render()
+
+    print("Taking second round of actions")
+    r.step({"player_1": (10, 1), "player_2": (10, 1)})
+    print(r.observe("player_1"))
+    print(r.env.terminations)
+    r.render()
+
+    print("Taking third round of actions")
+    r.step({"player_1": (10, 2), "player_2": (10, 2)})
+    print(r.observe("player_1"))
+    print(r.env.terminations)
+    r.render()
